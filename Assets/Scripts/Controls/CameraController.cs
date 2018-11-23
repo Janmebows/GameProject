936136ca8bee241d5b6a6d2970e7fa5769363635
Janmebows <janmebows@gmail.com>
2018-11-23 11:01:09 +1030

    More Camera stuff!
    Targetting is (almost) how we want it!
    Decided on a control scheme for in combat (will have to change some for out of combat)
    want to make LB/RB change target to the left/right

--- Assets/Scripts/Controls/CameraController.cs
+++ Assets/Scripts/Controls/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
 public class CameraController : MonoBehaviour
@@ -26,7 +27,8 @@ public class CameraController : MonoBehaviour
     //the offset for the camera to look over right(left) shoulder
     public Vector3 offset;
     //Whether or not the player has targetted an enemy
-    public bool lockOn;
+    public bool tryLockOn;
+    bool lockedOn;
     // Use this for initialization
     void Start()
     {
@@ -47,11 +49,29 @@ public class CameraController : MonoBehaviour
     {
         Vector3 position;
         Quaternion rotation;
-        //this condition will have to change to support if locking on is allowed
-        lockOn = lockOn ^ Input.GetButtonDown("XboxRightStickClick");
+        //do we want to be locked on?
+        tryLockOn = lockedOn ^ Input.GetButtonDown("XboxRightStickClick");
 
-        if (lockOn)
+        //don't want to be locked on
+        if (!tryLockOn)
         {
+            //stop being locked on
+            lockedOn = false;
+            //clear the target for future use
+            target = null;
+        }
+        //not yet locked on but we want to be
+        else if (tryLockOn && !lockedOn)
+        {
+            //see if there are any valid targets
+            //and need to find the 'best' target
+            FindBestTarget();
+            if (target != null)
+                lockedOn = true;
+
+        }
+
+        if (lockedOn && target!=null) { 
             rotation = transform.rotation;
             Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
             position = transform.rotation * negDistance + player.position + (transform.rotation * offset);
@@ -92,6 +112,54 @@ public class CameraController : MonoBehaviour
 
     }
 
+    //true if a target was found
+    //false otherwise
+    //sets the target within the script
+    bool FindBestTarget()
+    {
+        List<GameObject> validTargets = new List<GameObject>();
+        //may have a script which can manage this when it does spawning, etc.
+        //HAVE TO GIVE EVERY ENEMY A TAG THAT ACKNOWLEDGES THEM AS AN ENEMY
+        GameObject[] enemiesInScene = GameObject.FindGameObjectsWithTag("Enemy");
+        if(enemiesInScene.Length >= 1)
+        {
+            //check if it is a valid target
+            foreach (GameObject enemy in enemiesInScene)
+            {
+                if (enemy.GetComponentInChildren<Renderer>().IsVisibleFrom(Camera.main))
+                    validTargets.Add(enemy);
+            }
+            //if theres only one valid target - we lock on to that one
+            if(validTargets.Count==1)
+            {
+                target = validTargets[0].transform;
+                return true;
+            }
+            //if there are multiple targets pick the best
+            else if(validTargets.Count >1)
+            {
+                target = validTargets[0].transform;
+                foreach(GameObject obj in validTargets)
+                    target =BestOutOfTwoValidTargets(target, obj.transform);
+                return true;
+            }
+        }
+        //no targets were found
+        target = null;
+        return false;
+    }
+    //currently just looks at distance but i want it to prioritise centredness higher than distance (but consider both)
+    Transform BestOutOfTwoValidTargets(Transform target1, Transform target2)
+    {
+        float invLikelihood1 = Vector3.Magnitude(target1.position - transform.position);
+        float invLikelihood2 = Vector3.Magnitude(target2.position - transform.position);
+        if (invLikelihood1 <= invLikelihood2)
+            return target1;
+        else
+        {
+            return target2;
+        }
+    }
 
     //implements a zoomin effect
     void ZoomIn(ref float distance)
---
+++ Assets/Scripts/Controls/RendererExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class RendererExtensions
+{
+    //Checks if an object is seen by the samera
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
